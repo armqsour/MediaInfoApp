5e7e998e13b30467ceb21c3826b26ed09ac70dfb
armqsour <zzzyjdsg@qq.com>
2020-12-14 16:43:53 +0800

    first commit

---
+++ app/src/main/java/com/qs/mediainfoapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.qs.mediainfoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
